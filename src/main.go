18d59a7ce84e75d4dea1b8afa61bfcd4a989bee3
Your Name <you@example.com>
2019-07-24 22:32:52 +0800

    try listen

--- src/main.go
+++ src/main.go
@@ -1,21 +1,25 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/juzi5201314/coolq-http-api"
+	"github.com/juzi5201314/coolq-http-api/server"
 )
 
 func main() {
-	fmt.Print("hello world!")
 	api := cqhttp_go_sdk.Api("http://localhost:5700", "MAX8char")
-	m, err := api.SendPrivateMsg(630558072, "hello", false)
+	_, err := api.SendPrivateMsg(630558072, "hello", false)
 	if err != nil {
 		fmt.Print(err.Error())
 	}
-	mstr, err := json.Marshal(m)
-	if err != nil {
-		fmt.Print(err.Error())
+	s := server.StartListenServer(5700, "/")
+	s.ListenPrivateMessage(server.PrivateMessageListener(pm))
+	s.Listen()
+}
+
+func pm(sub_type string, message_id float64, user_id float64, message string, font float64) map[string]interface{} {
+	println("receive message: " + message + "，type is" + sub_type)
+	return map[string]interface{}{
+		"reply": "auto reply",
 	}
-	fmt.Print(mstr)
 }
