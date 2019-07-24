d14c4642e2d13d9b085e4d29a1d8849c8410a735
Your Name <you@example.com>
2019-07-24 23:21:35 +0800

    add group

--- src/main.go
+++ src/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/juzi5201314/coolq-http-api"
 	"github.com/juzi5201314/coolq-http-api/server"
+	"strings"
 )
 
 func main() {
@@ -14,12 +15,26 @@ func main() {
 	}
 	s := server.StartListenServer(8080, "/msg")
 	s.ListenPrivateMessage(server.PrivateMessageListener(pm))
+	s.ListenGroupMessage(server.GroupMessageListener(gm))
 	s.Listen()
 }
 
 func pm(sub_type string, message_id float64, user_id float64, message string, font float64) map[string]interface{} {
-	fmt.Print("receive message: " + message + "，type is" + sub_type)
-	return map[string]interface{}{
-		"reply": "auto reply",
+	//fmt.Print("receive message: " + message + "，type is" + sub_type)
+	if strings.Contains(message, "#") {
+		return map[string]interface{}{
+			"reply": strings.TrimPrefix(message, "#"),
+		}
 	}
+	return nil
+}
+
+func gm(sub_type string, message_id float64, group_id float64, user_id float64, anonymous string, anonymous_flag string, message string, font float64) map[string]interface{} {
+	fmt.Print("receive message: " + message + "，type is: " + sub_type + " \n")
+	if strings.Contains(message, "#") {
+		return map[string]interface{}{
+			"reply": message,
+		}
+	}
+	return nil
 }
