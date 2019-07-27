b18b4b2bbb808f45fec834fedba36d4a61341837
Your Name <you@example.com>
2019-07-27 18:29:10 +0800

    fix dj

--- src/main.go
+++ src/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"github.com/golang/glog"
 	"github.com/juzi5201314/coolq-http-api"
+	"github.com/juzi5201314/coolq-http-api/cq"
 	"github.com/juzi5201314/coolq-http-api/server"
 	"strings"
 )
@@ -38,6 +39,12 @@ func pm(sub_type string, message_id float64, user_id float64, message string, fo
 func gm(sub_type string, message_id float64, group_id float64, user_id float64, anonymous string, message string, font float64) map[string]interface{} {
 	glog.Info("receive message: " + message + "，type is: " + sub_type + " \n")
 	if message[0] == '#' {
+		if strings.Contains(message, "dj") {
+			return map[string]interface{}{
+				"reply":     strings.TrimPrefix(strings.Replace(message, "dj", cq.At("458830973"), -1), "#"),
+				"at_sender": false,
+			}
+		}
 		return map[string]interface{}{
 			"reply":     strings.TrimPrefix(message, "#"),
 			"at_sender": false,
